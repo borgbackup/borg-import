88337a97cd6d46b9f8b6a7d6d230afa79208fc35
Thomas Waldmann <tw@waldmann-edv.de>
2016-11-06 18:50:33 +0100

    move package dir to below src/, adapt toplevel script

--- borg-import.py
+++ borg-import.py
@@ -1,4 +1,11 @@
-#!borg-env/bin/python
+#!/usr/bin/env python
+# temporary hack until we have a setup.py that creates the main script
+
+import os
+import sys
+
+package_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'src'))
+sys.path.insert(0, package_path)
 
 from borg_import.main import main
 
