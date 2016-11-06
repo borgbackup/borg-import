6f99cff43ad34f4d1c95d87b5bca71e0bd86098c
Thomas Waldmann <tw@waldmann-edv.de>
2016-11-06 23:01:27 +0100

    directory discovery / filter / parser helper + tests

---
+++ src/borg_import/helpers/testsuite/test_discover.py
@@ -0,0 +1,22 @@
+import re
+from functools import partial
+
+from ..discover import discover, depth_filter, parser
+
+
+def test_discover(tmpdir):
+    root_dir = tmpdir.mkdir('root')
+    host1_dir = root_dir.mkdir('hostname1')
+    host1_dir.mkdir('0')
+    host1_dir.mkdir('1')
+    host2_dir = root_dir.mkdir('hostname2')
+    host2_dir.mkdir('3')
+    df = partial(depth_filter, max_depth=2)
+    dirs = list(discover(str(root_dir), df))
+    assert dirs == ['hostname1', 'hostname2', 'hostname1/0', 'hostname1/1', 'hostname2/3']
+
+
+def test_parser():
+    rx = re.compile(r'(?P<hostname>.+)/(?P<generation>.+)')
+    assert parser('host1/gen0', rx) == dict(hostname='host1', generation='gen0')
+    assert parser('foo', rx) is None
