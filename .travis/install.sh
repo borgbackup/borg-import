86084427d0591c8a0ff5c8ccd823c034390d30d1
Thomas Waldmann <tw@waldmann-edv.de>
2016-12-23 01:53:13 +0100

    add travis, tox, pytest, coverage, flake8 setup

---
+++ .travis/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -x
+
+sudo add-apt-repository -y ppa:costamagnagianfranco/borgbackup
+sudo apt-get update
+sudo apt-get install -y borgbackup
+
+pip install 'virtualenv<14.0'
+python -m virtualenv ~/.venv
+source ~/.venv/bin/activate
+pip install -r requirements.d/development.txt
+pip install codecov
+pip install -e .
