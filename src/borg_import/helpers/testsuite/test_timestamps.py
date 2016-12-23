60d85e3cb470f74a61edb6c38d41e7eb0cd058d7
Thomas Waldmann <tw@waldmann-edv.de>
2016-12-23 03:15:44 +0100

    fix timezone for travis

--- src/borg_import/helpers/testsuite/test_timestamps.py
+++ src/borg_import/helpers/testsuite/test_timestamps.py
@@ -20,7 +20,7 @@ def test_datetime_from_mtime(tmpdir):
 def test_datetime_from_string():
     assert datetime_from_string('1999-12-31T23:59:59') == \
            datetime(1999, 12, 31, 23, 59, 59)
-    assert datetime_from_string('Mon Oct 31 23:35:50 CET 2016') == \
+    assert datetime_from_string('Mon Oct 31 23:35:50 UTC 2016') == \
            datetime(2016, 10, 31, 23, 35, 50)
     with pytest.raises(ValueError):
         datetime_from_string('total crap')
