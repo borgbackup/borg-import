21ee466c945b771fbe2c566ae825116ee8686695
Albert Huang <alberthdev@users.noreply.github.com>
2019-10-28 20:24:38 -0400

    Fix virtualenv version to allow Python 3.8 compat
    Actual problem was with the virtualenv version - <14.0 does not
    contain the latest pip, hence the breakage. <14.0 doesn't seem
    to make sense here, since we're not supporting Python 3.2 anymore.
    In the rare case that Ubuntu Xenial's version is used (it shouldn't,
    Travis will usually override that), it also ships with Python 3.5,
    so this should be OK.
    
    Removed older logic of using get-pip.py - no longer needed.

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,13 +7,7 @@ sudo add-apt-repository -y ppa:costamagnagianfranco/borgbackup
 sudo apt-get update
 sudo apt-get install -y fakeroot borgbackup
 
-# Chicken/egg problem with Python 3.8... current Travis pip is not
-# compatible with Python 3.8, but we need a newer one to fix it...
-# Solution: install pip the "manual" way so that we can bootstrap.
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py --no-setuptools --no-wheel
-
-pip install 'virtualenv<14.0'
+pip install 'virtualenv=16.7.7'
 python -m virtualenv ~/.venv
 source ~/.venv/bin/activate
 pip install -r requirements.d/development.txt
