068e15d09cce8470f0d4c1f1345160c661619f4e
Albert Huang <alberthdev@users.noreply.github.com>
2019-10-28 20:29:50 -0400

    Fix version spec typo for virtualenv install - '=' -> '=='

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,7 +7,7 @@ sudo add-apt-repository -y ppa:costamagnagianfranco/borgbackup
 sudo apt-get update
 sudo apt-get install -y fakeroot borgbackup
 
-pip install 'virtualenv=16.7.7'
+pip install 'virtualenv==16.7.7'
 python -m virtualenv ~/.venv
 source ~/.venv/bin/activate
 pip install -r requirements.d/development.txt
