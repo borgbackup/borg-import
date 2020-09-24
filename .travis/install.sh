46a1bb288573913b2d7e581a0d5be47b5bf523e2
Albert Huang <alberthdev@users.noreply.github.com>
2020-09-24 11:42:48 +0000

    Attempt to extend Ubuntu coverage to later versions + tidy up travis-ci cfg

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,14 +3,19 @@
 set -e
 set -x
 
-sudo add-apt-repository -y ppa:costamagnagianfranco/borgbackup
-sudo apt-get update
-
 is_xenial=$(lsb_release -r | grep 16.04 &>/dev/null && echo "y" || echo "n")
+is_focal=$(lsb_release -r | grep 20.04 &>/dev/null && echo "y" || echo "n")
 
+if [ "$is_focal" = "n" ]; then
+    # Only add PPA if we're not testing for Focal
+    # We technically don't use the PPA for Xenial either, but keeping it for now should be OK
+    sudo add-apt-repository -y ppa:costamagnagianfranco/borgbackup
+fi
+
+sudo apt-get update
 sudo apt-get install -y fakeroot
 
-if [ "$is_xenial" = "y" ]; then
+if [ "$is_xenial" = "y" ] || [ "$is_focal" = "y" ]; then
     # Workaround for broken PPA
     sudo wget -O /usr/bin/borg https://github.com/borgbackup/borg/releases/download/1.1.11/borg-linux64
 else
