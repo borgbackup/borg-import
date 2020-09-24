2626e9ddb99cf83cd8bf4ee2483c4e9772628e81
Albert Huang <alberthdev@users.noreply.github.com>
2020-09-24 01:32:29 +0000

    Attempt to fix broken build on travis-ci for xenial

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,7 +5,17 @@ set -x
 
 sudo add-apt-repository -y ppa:costamagnagianfranco/borgbackup
 sudo apt-get update
-sudo apt-get install -y fakeroot borgbackup
+
+is_xenial=$(lsb_release -r | grep 16.04 &>/dev/null && echo "y" || echo "n")
+
+sudo apt-get install -y fakeroot
+
+if [ "$is_xenial" = "y" ]; then
+    # Workaround for broken PPA
+    sudo wget -O /usr/bin/borg https://github.com/borgbackup/borg/releases/download/1.1.11/borg-linux64
+else
+    sudo apt-get install -y borgbackup
+fi
 
 pip install 'virtualenv'
 python -m virtualenv ~/.venv
