6404127560a09d927056e76fc04c02cd50c7516a
Albert Huang <alberthdev@users.noreply.github.com>
2020-10-15 02:24:30 +0000

    Allow PPA borgbackup install on xenial+focal

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,21 +6,9 @@ set -x
 is_xenial=$(lsb_release -r | grep 16.04 &>/dev/null && echo "y" || echo "n")
 is_focal=$(lsb_release -r | grep 20.04 &>/dev/null && echo "y" || echo "n")
 
-if [ "$is_focal" = "n" ]; then
-    # Only add PPA if we're not testing for Focal
-    # We technically don't use the PPA for Xenial either, but keeping it for now should be OK
-    sudo add-apt-repository -y ppa:costamagnagianfranco/borgbackup
-fi
-
+sudo add-apt-repository -y ppa:costamagnagianfranco/borgbackup
 sudo apt-get update
-sudo apt-get install -y fakeroot
-
-if [ "$is_xenial" = "y" ] || [ "$is_focal" = "y" ]; then
-    # Workaround for broken PPA
-    sudo wget -O /usr/bin/borg https://github.com/borgbackup/borg/releases/download/1.1.11/borg-linux64
-else
-    sudo apt-get install -y borgbackup
-fi
+sudo apt-get install -y fakeroot borgbackup
 
 pip install 'virtualenv'
 python -m virtualenv ~/.venv
