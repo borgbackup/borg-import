5c308da0a9d8e5dbd2c7bd9ba41a022c1023a0d8
Albert Huang <alberthdev@users.noreply.github.com>
2020-10-27 00:35:01 +0000

    Update with less targets + cleaner script

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,9 +3,6 @@
 set -e
 set -x
 
-is_xenial=$(lsb_release -r | grep 16.04 &>/dev/null && echo "y" || echo "n")
-is_focal=$(lsb_release -r | grep 20.04 &>/dev/null && echo "y" || echo "n")
-
 sudo add-apt-repository -y ppa:costamagnagianfranco/borgbackup
 sudo apt-get update
 sudo apt-get install -y fakeroot borgbackup
