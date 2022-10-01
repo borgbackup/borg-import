8f1c503fe6947d04fc020d3f26d49f104a796ac8
Thomas Waldmann <tw@waldmann-edv.de>
2022-10-01 15:04:54 +0200

    add python 3.10 support

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: System :: Archiving :: Backup',
     ],
     packages=find_packages('src'),
